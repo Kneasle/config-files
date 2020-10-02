c93572e3226a74d2175656cd1d50afd7d0cc4b67
Kneasle <kneasle@gmail.com>
2020-10-02 16:54:37 +0100

    Fix mistakes in fish config

--- config.fish
+++ config.fish
@@ -11,13 +11,13 @@ end
 alias new_pwd="pwgen -Ays 16 5 -C1"
 
 # ===== CONVENIENT SHORTHANDS FOR DIRECTORIES =====
-set -l DOCUMENTS ~/documents
+set DOCUMENTS ~/documents
 set PROGRAMMING $DOCUMENTS/programming
 
 # Language directories
 set PY $PROGRAMMING/python
 set RUST $PROGRAMMING/rust
-set -l MLP $PROGRAMMING/multi-language-projects
+set MLP $PROGRAMMING/multi-language-projects
 
 # Python projects
 set WHEATLEY $PY/ringing/wheatley
@@ -30,9 +30,9 @@ set SAPLING $RUST/sapling
 
 # Multi-language projects
 set METAIMAGE $MLP/meta-image
-set TOWER_MAP $MPL/bellringers-world-map
-set RINGING_ROOM $MPL/ringing-room/virtual-ringing-room
-set TANK_TROUBLE $MPL/tank-trouble
+set TOWER_MAP $MLP/bellringers-world-map
+set RINGING_ROOM $MLP/ringing-room/virtual-ringing-room
+set TANK_TROUBLE $MLP/tank-trouble
 
 # Others
 set SPLIT $PROGRAMMING/html+js/split
