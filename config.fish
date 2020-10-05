49034787257872332d3845008c17e1f94c13f1b2
Kneasle <kneasle@gmail.com>
2020-10-05 16:31:28 +0100

    Sort config.fish and add CROUPIER

--- config.fish
+++ config.fish
@@ -15,29 +15,30 @@ set DOCUMENTS ~/documents
 set PROGRAMMING $DOCUMENTS/programming
 
 # Language directories
+set CONFIG_FILES $PROGRAMMING/config-files
+set MLP $PROGRAMMING/multi-language-projects
 set PY $PROGRAMMING/python
 set RUST $PROGRAMMING/rust
-set MLP $PROGRAMMING/multi-language-projects
-set CONFIG_FILES $PROGRAMMING/config-files
 
 # Python projects
 set WHEATLEY $PY/ringing/wheatley
 
 # Rust projects
 set BELLMETAL $RUST/bellmetal/bellmetal
+set CROUPIER $RUST/croupier
 set HERMIONE $RUST/hermione
-set WIKI_LINKS $RUST/wiki-links
 set SAPLING $RUST/sapling
+set WIKI_LINKS $RUST/wiki-links
 
 # Multi-language projects
 set METAIMAGE $MLP/meta-image
-set TOWER_MAP $MLP/bellringers-world-map
 set RINGING_ROOM $MLP/ringing-room/virtual-ringing-room
 set TANK_TROUBLE $MLP/tank-trouble
+set TOWER_MAP $MLP/bellringers-world-map
 
 # Others
-set SPLIT $PROGRAMMING/html+js/split
 set COMPLIB $PROGRAMMING/complib/src
+set SPLIT $PROGRAMMING/html+js/split
 
 # Make RR always use my local Wheatley repository for testing purposes
 set RR_WHEATLEY_PATH "$WHEATLEY/run-wheatley"
